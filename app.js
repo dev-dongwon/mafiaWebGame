bccccc27af301a75610917dff5b931f71293b62e
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-06 19:52:31 +0900

    Feat : index, error 함수를 미들웨어에 추가
    index 요청 및 error 처리 함수 추가

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const App = require('./src/application');
-const debug = require('./utils/debug')('app')
+const debug = require('./utils/debug')('app');
+const path = require('path');
+const fs = require('fs');
+const serveStatic = require('./src/serve-static');
 const app = new App();
+
+const index = (req, res, next) => {
+  const publicPath = path.join(__dirname, './public')
+  fs.readFile(`${publicPath}/index.html`, (err, data) => {
+    if (err) throw err
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/html')
+    res.end(data)
+  })
+}
+
+const error404 = (req, res, next) => {
+  res.statusCode = 404
+  res.end('Not Found')
+}
+
+const error = (err, req, res, next) => {
+  res.statusCode = 500
+  res.end()
+}
+
+app.use(serveStatic());
+app.use(index);
+app.use(error404)
+app.use(error)
 
 debug('app이 실행되었습니다')
 module.exports = app;
