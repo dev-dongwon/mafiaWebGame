dbda13f4fcc59d7ce92c1b9d9c7ccd0c23be79a2
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-06 21:04:25 +0900

    Feat : 요청 메서드에 따라 logger 색상 변환
    메서드 종류에 따라 색상을 달리해 log 출력

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,12 +1,20 @@
 const color =  {
   yellowColor: '\x1b[33m',
   greenColor: '\x1b[32m',
+  cyanColor: '\x1b[36m',
   redColor: '\x1b[31m',
   resetColor: '\x1b[0m',
 };
 
+const getMethodColor = {
+  get : color.greenColor,
+  post : color.yellowColor,
+  put : color.cyanColor,
+  delete: color.redColor,
+}
+
 const logger = () => (req, res, next) => {
-  const log = `${color.yellowColor}[${req.method}]${color.resetColor} ${req.url}`
+  const log = `${getMethodColor[req.method.toLowerCase()]}[${req.method}]${color.resetColor} ${req.url}`
   console.log(log);
   next();
 }
