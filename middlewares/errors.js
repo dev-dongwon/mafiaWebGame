4d310ccd1af8b5c64c3a9a98079880e5fc7dba3b
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-06 21:07:53 +0900

    Modified : 에러 미들웨어에 응답 메시지 추가
    404 에러와 500 에러 응답 메시지 추가

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,11 +1,11 @@
 const error404 = () => (req, res, next) => {
-  res.statusCode = 404
-  res.end('Not Found')
+  res.statusCode = 404;
+  res.end('404 : Not Found page');
 }
 
 const error = () => (err, req, res, next) => {
   res.statusCode = 500
-  res.end()
+  res.end('500 : Internal server error');
 }
 
 module.exports = {
