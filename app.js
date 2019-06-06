31a0fbc3b1aab4713afe754011446ea56fa8882a
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-06 15:29:16 +0900

    Feat : debug 모듈 require 후 적용

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const App = require('./src/application');
+const debug = require('./utils/debug')('app')
 const app = new App();
 
+debug('app이 실행되었습니다')
 module.exports = app;
--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,8 @@
 const app = require('../app');
+const debug = require('../utils/debug')('bin/www')
 const host = 'localhost';
 const port = 3000;
 
 app.listen(port, host, () => {
-  console.log(`server running at http://${host}:${port}`);
+  debug(`server running at http://${host}:${port}`);
 });
