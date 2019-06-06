2f244197efe621043395cbcefce21ab1969b6477
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-06 18:23:51 +0900

    Feat : application 모듈 생성자에 미들웨어 모듈 추가
    서버 생성 시 미들웨어가 실행되도록 생성자 추가

--- src/application.js
+++ src/application.js
@@ -1,17 +1,11 @@
 const http = require('http');
-const path = require('path');
-const fs = require('fs');
+const Middleware = require('./middleware');
 
 const Application = class {
   constructor() {
+    this.middleware = Middleware();
     this.server = http.createServer((req, res) => {
-
-      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-      const filePath = path.join(__dirname, '../public/index.html');
-      fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) throw err
-        res.end(data);
-      })
+      this.middleware.run(req, res);
     })
   }
 
