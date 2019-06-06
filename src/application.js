d98c407d49e7d6d5e284258f62ffb519c61c4902
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-06 15:51:38 +0900

    Feat : 정적 파일 index.html 읽을 수 있도록 server 프로퍼티 수정
    path 모듈과 fs 모듈을 이용해 index.html 파일을 읽어
    res 객체로 응답하도록 수정
    todo : 다른 html 파일과 정적 파일을 읽을 수 있도록 리팩토링 필요

--- src/application.js
+++ src/application.js
@@ -1,17 +1,23 @@
 const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
 const Application = class {
   constructor() {
     this.server = http.createServer((req, res) => {
-      res.setHeader('Content-Type', 'text/plain');
-      res.write('hello, node')
-      res.end();
+
+      res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+      const filePath = path.join(__dirname, '../public/index.html');
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) throw err
+        res.end(data);
+      })
     })
   }
 
-  listen(port=3000, host='localhost', func) {
+  listen(port = 3000, host = 'localhost', func) {
     this.server.listen(port, host, func);
   }
-}  
+}
 
 module.exports = Application;
