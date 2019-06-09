09492c7d245a515fa9f87501c32a5cf2307b31a8
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-09 16:54:15 +0900

    Create : ajax 요청 시 처리할 메시지 목록 객체

---
+++ utils/message-list.js
@@ -0,0 +1,8 @@
+const messageList = {
+    id : {
+      checkIdSuccess : `<p style="color:Aquamarine">사용할 수 있는 ID입니다</p>`,
+      checkIdFail : `<p style="color:red">사용할 수 없는 ID입니다</p>`
+    }
+  }
+
+  module.exports = messageList;
