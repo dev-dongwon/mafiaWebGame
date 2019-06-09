7162db5b0fa11c661b4ca5b4265add5baf84c9b2
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-09 20:37:18 +0900

    Feat : id 체크 유형별로 메시지 추가
    가능한 경우, 정규표현식 매칭이 안되는 경우, 중복된 경우로 나눠서 메시지
    추가

--- utils/message-list.js
+++ utils/message-list.js
@@ -1,7 +1,8 @@
 const messageList = {
     id : {
-      checkIdSuccess : `<p style="color:Aquamarine">사용할 수 있는 ID입니다</p>`,
-      checkIdFail : `<p style="color:red">사용할 수 없는 ID입니다</p>`
+      isAvailable : `<p style="color:Aquamarine">사용할 수 있는 ID입니다</p>`,
+      notMatchedReg : `<p style="color:red">사용할 수 없는 ID입니다</p>`,
+      isDuplicated : `<p style="color:red">이미 존재하는 아이디입니다</p>`
     }
   }
 
