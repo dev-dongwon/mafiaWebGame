0f892bf2318e8401305232df5d19660a40356874
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-16 13:55:21 +0900

    Feat : 회원가입 password 체크 후 ajax 메시지 출력
    password 조건 4자리 미만일 경우 정규식과 매칭하여 메시지 출력

--- public/js/join.js
+++ public/js/join.js
@@ -1,4 +1,5 @@
-let inputIdBox = document.getElementById('inputId');
+const inputIdBox = document.getElementById('inputId');
+
 const checkIdAjax = () => inputIdBox.addEventListener('keyup', () => {
   const url = `http://localhost:3000/register/id?value=${inputIdBox.value}`;
   
@@ -13,10 +14,44 @@ const checkIdAjax = () => inputIdBox.addEventListener('keyup', () => {
     const ajaxTexst = await response.text();
     document.getElementById('ajax-checkId').innerHTML = ajaxTexst;
   }, 500);
-
 })
 
-// todo : 비밀번호 정규표현식 매칭
+// 비밀번호 정규표현식 매칭
+const inputPassword = document.getElementById('inputPassword');
+const checkPassword =  document.getElementById('checkPassword');
+
+inputPassword.addEventListener('keyup', () => {
+  const ajaxBox = document.getElementById('ajax-checkPassword');
+  const isValid = isValidPassword();
+
+  if (isValid) {
+    ajaxBox.innerHTML = `<p>올바른 비밀번호입니다</p>`
+  } else {
+    ajaxBox.innerHTML = `<p>비밀번호는 4자리 이상입니다</p>`
+  }
+})
+
+checkPassword.addEventListener('keyup', () => {
+  const checkPasswordBox = document.getElementById('ajax-isValidPassword');
+  const isValid = isSamePassword();
+
+  if (isValid) {
+    checkPasswordBox.innerHTML = `<p>비밀번호가 일치합니다</p>`
+    checkPasswordBox.firstChild.style.color = 'cyan';
+  } else {
+    checkPasswordBox.innerHTML = `<p>비밀번호가 일치하지 않습니다</p>`
+    checkPasswordBox.firstChild.style.color = 'red';
+  }
+})
+
+const isSamePassword = () => {
+  return inputPassword.value === checkPassword.value;
+}
+
+const isValidPassword = () => {
+  const passwordReg = /(?=.{4,})/;
+  return passwordReg.test(inputPassword.value);
+}
 
 const registerEvent = () => {
   checkIdAjax();
