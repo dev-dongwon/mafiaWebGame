f8b1cc04fa904227737bbf916fcd20263a02f3d9
dev-dongwon <dev.dongwonkim@gmail.com>
2019-06-16 14:09:20 +0900

    Feat : 회원 정보 객체를 가져오는 메서드 추가
    id를 키로 가지는 회원정보 객체 가져오는 기능 추가

--- routers/login/controller.js
+++ routers/login/controller.js
@@ -3,6 +3,20 @@ const cryptoPassword = require('../../utils/crypto-password');
 
 const loginController = {
 
+  async getMemberDataObj() {
+    return await csvParser.readCsvData('./db/member.csv', csvParser.getMemberObj);
+  },
+
+  getMemberObj(memberObj, inputId) {
+    let member = null;
+    Object.keys(memberObj).forEach((key) => {
+      if (key === inputId) {
+        member = memberObj[key];
+      }
+    })
+    return member;
+  },
+
 }
 
 module.exports = loginController;
